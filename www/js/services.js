2c9371184eeb42ae259ee12208d562fa50bf7823
mju <mju@mjuui-MacBook-Air-2.local>
2016-06-09 09:14:51 +0900

    service경로 heroku로 변경

--- www/js/services.js
+++ www/js/services.js
@@ -1,17 +1,17 @@
 angular.module('recipeGo.services', ['ngResource'])
 
     .factory('Ingredients', function ($resource) {
-        return $resource('/ingredients');
+        return $resource('https://gentle-shelf-38663.herokuapp.com/ingredients');
     })
     .factory('Recipes', function($resource) {
-    	return $resource('/recipes/:recipeId');
+    	return $resource('https://gentle-shelf-38663.herokuapp.com/recipes/:recipeId');
     })
     .factory('Like', function($resource) {
-    	return $resource('/likes');
+    	return $resource('https://gentle-shelf-38663.herokuapp.com/likes');
     })
     .factory('Main', function($resource) {
-    	return $resource('/main');
+    	return $resource('https://gentle-shelf-38663.herokuapp.com/main');
     })
     .factory('Vision', function($resource) {
-    	return $resource('/vision');
+    	return $resource('https://gentle-shelf-38663.herokuapp.com/vision');
     });
