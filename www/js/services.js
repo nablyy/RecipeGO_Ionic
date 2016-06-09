855ae051ac192c017644c8565c52deab6f85ea7b
mju <mju@mjuui-MacBook-Air-2.local>
2016-06-09 15:27:06 +0900

    d

--- www/js/services.js
+++ www/js/services.js
@@ -1,17 +1,17 @@
 angular.module('recipeGo.services', ['ngResource'])
 
     .factory('Ingredients', function ($resource) {
-        return $resource('https://gentle-shelf-38663.herokuapp.com/ingredients');
+        return $resource('https://ancient-escarpment-45607.herokuapp.com/ingredients');
     })
     .factory('Recipes', function($resource) {
-    	return $resource('https://gentle-shelf-38663.herokuapp.com/recipes/:recipeId');
+    	return $resource('https://ancient-escarpment-45607.herokuapp.com/recipes/:recipeId');
     })
     .factory('Like', function($resource) {
-    	return $resource('https://gentle-shelf-38663.herokuapp.com/likes');
+    	return $resource('https://ancient-escarpment-45607.herokuapp.com/likes');
     })
     .factory('Main', function($resource) {
-    	return $resource('https://gentle-shelf-38663.herokuapp.com/main');
+    	return $resource('https://ancient-escarpment-45607.herokuapp.com/main');
     })
     .factory('Vision', function($resource) {
-    	return $resource('https://gentle-shelf-38663.herokuapp.com/vision');
+    	return $resource('https://ancient-escarpment-45607.herokuapp.com/vision');
     });
